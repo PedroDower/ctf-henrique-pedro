e41e8b95147ab743604179a161039d8e017f2c93
Pedro Dower <pedrodowerdev@gmail.com>
2023-01-03 20:24:24 -0300

    restore old client engine

--- public/client-engine.js
+++ public/client-engine.js
@@ -1,24 +1,57 @@
-class ClientEngine {
-  sprites = [];
-  isOn = false;
-
+export default class ClientEngine {
+    
   constructor(canvasElement) {
-    const context = canvasElement.getContext("2d");
-    context.fillStyle = "white";
-    context.clearRect(0, 0, 10, 20);
+    this.context = canvasElement.getContext("2d");
+    
+    this.sprites = [];
+    this.isOn = false;
+    this.frameRate = 1000 / 5; // in miliseconds
   }
 
   start() {
     this.isOn = true;
-
-    window.requestAnimationFrame(this.nextFrame);
+    this.nextFrame();
   }
 
   stop() {
     this.isOn = false;
   }
 
-  draw() {
+  nextFrame() {
+    if (!this.isOn) {
+      return;
+    }
+
+    if (this.isNewFrameTime()) {
+      this.drawNewFrame();
+    }
+
+    window.requestAnimationFrame(() => {
+      this.nextFrame();
+    });
+  }
+
+  isNewFrameTime() {
+    const now = new Date().getTime();
+
+    if (!this.lastTime) this.lastTime = now;
+
+    if (now - this.lastTime >= this.frameRate) {
+      this.lastTime = now;
+      return true;
+    }
+
+    return false;
+  }
+
+  clearScreen() {
+    this.context.fillStyle = "white";
+    this.context.clearRect(0, 0, 10, 20);
+  }
+
+  drawNewFrame() {
+    this.clearScreen();
+
     for (let sprite of this.sprites) {
       this.sprite.draw();
     }
